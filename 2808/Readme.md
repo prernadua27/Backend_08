77bfd69e538d4205f993e0452213f603ae874888
prernadua27 <prerna680.be22@chitkara.edu.in>
2024-08-31 20:31:30 +0530

    Mongo

---
+++ 2808/Readme.md
@@ -0,0 +1,3 @@
+mongodb uses bson bson faster than json
+
+commands mongosh show dbs use chitkara db show dbs
---
+++ 2808/code.js
@@ -0,0 +1,76 @@
+const students = [
+    {
+    name: "Prerna",
+    age: 20,
+    marks:45,
+    subject: "English"
+    },
+    {
+    name: "John",
+    age: 22,
+    marks: 50,
+    subject: "Math"
+    },
+    {
+      name: "Pranya",
+      age: 21,
+      marks: 48,
+      subject: "Science"
+    },
+    {
+      name: "Michael",
+      age: 20,
+      marks: 42,
+      subject: "History" 
+    },
+    {
+      name: "Pratham",
+    age: 23,
+    marks: 56,
+    subject: "Computer Science"},
+    {
+      name: "Himani",
+      age: 24,
+      marks: 52,
+      subject: "Maths"
+      },
+    {name: "Ram",
+      age: 25,
+      marks: 58,
+      subject: "English"}
+    
+  ]
+
+
+  const faculty=[
+  {
+    name: "Palak",
+    age: 20, 
+    subject: "Hindi", 
+    rating: 5
+  },
+  {
+    name: "Poorva" ,
+    age: 20,
+    subject: "English",
+    rating: 5
+  },
+  {
+    name: "Parul",
+    age: 20, 
+    subject: "Math",
+    rating: 5
+  },
+  {
+    name: "Michael", 
+    age: 20, 
+    subject: "English", 
+    rating: 5
+  },
+  {
+    name: "Sarah",
+     age: 20, 
+     subject: "Science",
+      rating: 5
+    }
+  ]
